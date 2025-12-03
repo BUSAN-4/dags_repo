350ae1c76755740bc044d843aa5c446d364d02ec
kjc <wjdcks524@naver.com>
2025-12-03 13:02:39 +0900

    :feature: driving_session table 추가

--- flink_sql/test.sql
+++ flink_sql/test.sql
@@ -2,13 +2,21 @@ SET 'execution.runtime-mode'='batch';
 SET 'sql-client.execution.result-mode'='tableau';
 SET 'parallelism.default'='1';
 SET 'pipeline.name' = 'mariadb-flink';
-CREATE CATALOG mariadb_catalog WITH (
-    'type' = 'jdbc',
+CREATE TABLE driving_session(
+    `session_id` STRING,
+    `car_id` STRING,
+    `start_time` TIMESTAMP(3),
+    `end_time` TIMESTAMP(3),
+    `create_at` TIMESTAMP(3),
+    `update_at` TIMESTAMP(3)
+) WITH (
+    'connector' = 'jdbc',
     'default-database' = 'car_db',
     'username' = 'root',
     'password' = 'busan!234pw',
-    'base-url' = 'jdbc:mysql://busan-maria.cf8s8geeaqc9.ap-northeast-2.rds.amazonaws.com:23306'
+    'url' = 'jdbc:mysql://busan-maria.cf8s8geeaqc9.ap-northeast-2.rds.amazonaws.com:23306'
 );
-SHOW CATALOGS;
-USE CATALOG mariadb_catalog;
-SHOW DATABASES;
+
+SELECT *
+FROM driving_session
+LIMIT 10;
