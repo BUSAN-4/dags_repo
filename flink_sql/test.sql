8f89fd2fe067ccff2ec207ff6df433681afa6c0c
kjc <wjdcks524@naver.com>
2025-12-03 13:15:35 +0900

    :feature: use a catalog in flink

--- flink_sql/test.sql
+++ flink_sql/test.sql
@@ -1,22 +1,40 @@
-SET 'execution.runtime-mode'='batch';
-SET 'sql-client.execution.result-mode'='tableau';
-SET 'parallelism.default'='1';
+SET execution.runtime-mode=batch;
+SET sql-client.execution.result-mode=tableau;
+SET 'parallelism.default'=1;
 SET 'pipeline.name' = 'mariadb-flink';
-CREATE TABLE driving_session(
-    `session_id` STRING,
-    `car_id` STRING,
-    `start_time` TIMESTAMP(3),
-    `end_time` TIMESTAMP(3),
-    `create_at` TIMESTAMP(3),
-    `update_at` TIMESTAMP(3)
-) WITH (
-    'connector' = 'jdbc',
+CREATE CATALOG mariadb_catalog WITH (
+    'type' = 'jdbc',
     'default-database' = 'car_db',
     'username' = 'root',
     'password' = 'busan!234pw',
-    'url' = 'jdbc:mysql://busan-maria.cf8s8geeaqc9.ap-northeast-2.rds.amazonaws.com:23306'
+    'base-url' = 'jdbc:mariadb://busan-maria.cf8s8geeaqc9.ap-northeast-2.rds.amazonaws.com:23306'
 );
+SHOW CATALOGS;
+USE CATALOG mariadb_catalog;
+SHOW DATABASES;
+USE car_db;
+SHOW TABLES;
 
-SELECT *
-FROM driving_session
-LIMIT 10;
+
+-- SET 'execution.runtime-mode'='batch';
+-- SET 'sql-client.execution.result-mode'='tableau';
+-- SET 'parallelism.default'='1';
+-- SET 'pipeline.name' = 'mariadb-flink';
+-- CREATE TABLE driving_session(
+--     `session_id` STRING,
+--     `car_id` STRING,
+--     `start_time` TIMESTAMP(3),
+--     `end_time` TIMESTAMP(3),
+--     `create_at` TIMESTAMP(3),
+--     `update_at` TIMESTAMP(3)
+-- ) WITH (
+--     'connector' = 'jdbc',
+--     'default-database' = 'car_db',
+--     'username' = 'root',
+--     'password' = 'busan!234pw',
+--     'url' = 'jdbc:mysql://busan-maria.cf8s8geeaqc9.ap-northeast-2.rds.amazonaws.com:23306'
+-- );
+
+-- SELECT *
+-- FROM driving_session
+-- LIMIT 10;
